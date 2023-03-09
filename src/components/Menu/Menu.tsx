2f2b1e7d6208251dc235f26a2056cdeb9e70932b
carlsylvan <carlsylvan@gmail.com>
2023-03-09 10:34:34 +0100

    added menu asset

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,40 +1,28 @@
+import { Course, menu, MenuSection } from "../../assets/menu";
+
 export const Menu = () => {
-
-    // interface MenuItem {
-    //     name: string;
-    //     description: string;
-    //     price: number;
-    //   }
-      
-    //   interface MenuSection {
-    //     name: string;
-    //     items: MenuItem[];
-    //   }
-      
-    //   interface Menu {
-    //     firstCourses: MenuSection;
-    //     mainCourses: MenuSection;
-    //     desserts: MenuSection;
-    //   }
-
-    //   let menu: Menu = [{}]
-
+  
     return (
-        <>
-            <h1>LAST DANCE</h1>
-            <h2>RESTAURANG</h2>
-            <div className="menu">
-                <h3>Menu</h3>
-                <h4>Förrätt</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quasi?</p>
-                <div>199kr</div>
-                <h4>Varmrätt</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quasi?</p>
-                <div>349kr</div>
-                <h4>Efterrätt</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quasi?</p>
-                <div>149kr</div>
+      <>
+        <h1>LAST DANCE</h1>
+        <h2>RESTAURANG</h2>
+        <div>
+          <h3>Menu</h3>
+          {Object.values(menu).map((section: MenuSection) => (
+            <div key={section.name}>
+              <h4>{section.name}</h4>
+              <ul>
+                {section.courses.map((course: Course) => (
+                  <li key={course.name}>
+                    <h5>{course.name}</h5>
+                    <p>{course.description}</p>
+                    <div>{course.price}kr</div>
+                  </li>
+                ))}
+              </ul>
             </div>
-        </>
-    )
-}
+          ))}
+        </div>
+      </>
+    );
+  };
