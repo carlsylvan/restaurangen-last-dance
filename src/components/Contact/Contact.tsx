2bce754a5184ea03e13585a27a8d45481ce00884
carlsylvan <carlsylvan@gmail.com>
2023-03-09 12:17:52 +0100

    added styled components to contact and menu

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,8 +1,15 @@
 import styled from "styled-components";
+import { Button, Form, Input, InputWrapper, Label } from "../styled/Booking";
 
 const Textarea = styled.textarea`
   width: 70vw;
   height: 40vh;
+  outline: none;
+  border-radius: 18px;
+  outline-color: black;
+  color: black;
+  border: 6px solid black;
+  font-size: 16px;
 `;
 
 export const Contact = () => {
@@ -11,25 +18,25 @@ export const Contact = () => {
         <h1>LAST DANCE</h1>
         <h2>RESTAURANG</h2>
         <h3>Kontakta oss</h3>
-        <form>
-            <div>
-                <label>Namn</label>
-                <input type="text" required />
-            </div>
-            <div>
-                <label>Email</label>
-                <input type="email"required />
-            </div>
-            <div>
-                <label>Bokningsnummer</label>
-                <input type="text" required />
-            </div>
-            <div>
-                <label>Meddelande</label>
+        <Form>
+            <InputWrapper>
+                <Label>Namn</Label>
+                <Input type="text" required />
+            </InputWrapper>
+            <InputWrapper>
+                <Label>Email</Label>
+                <Input type="email"required />
+            </InputWrapper>
+            <InputWrapper>
+                <Label>Bokningsnummer</Label>
+                <Input type="text" required />
+            </InputWrapper>
+            <InputWrapper>
+                <Label>Meddelande</Label>
                 <Textarea required></Textarea>
-            </div>
-            <button type="submit">Send</button>
-        </form>
+            </InputWrapper>
+            <Button type="submit">Send</Button>
+        </Form>
         </>
     )
 };
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,12 @@
 import { Course, menu, MenuSection } from "../../assets/menu";
+import { H1, H2 } from "../styled/Booking";
 
 export const Menu = () => {
   
     return (
       <>
-        <h1>LAST DANCE</h1>
-        <h2>RESTAURANG</h2>
+        <H1>LAST DANCE</H1>
+        <H2>RESTAURANG</H2>
         <div>
           <h3>Menu</h3>
           {Object.values(menu).map((section: MenuSection) => (
