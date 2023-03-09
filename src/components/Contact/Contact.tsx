5663304294a0feb8ce4ccf7f378ca71c0cfaa649
carlsylvan <carlsylvan@gmail.com>
2023-03-09 11:04:27 +0100

    styled textarea

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+
+const Textarea = styled.textarea`
+  width: 70%;
+  height: 70%;
+`;
+
 export const Contact = () => {
-    return <div>Hello Contact</div>
-}
+    return (
+        <form>
+            <div>
+                <label>Namn</label>
+                <input type="text" required />
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="email"required />
+            </div>
+            <div>
+                <label>Meddelande</label>
+                <Textarea required></Textarea>
+            </div>
+            <button type="submit">Send</button>
+        </form>
+    )
+};
