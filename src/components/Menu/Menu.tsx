eef9de3a5fca9f675b76a004834003f65ebea4d3
carlsylvan <carlsylvan@gmail.com>
2023-03-09 10:02:39 +0100

    added menu html

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,3 +1,40 @@
 export const Menu = () => {
-    return <div>Hello Menu</div>
+
+    // interface MenuItem {
+    //     name: string;
+    //     description: string;
+    //     price: number;
+    //   }
+      
+    //   interface MenuSection {
+    //     name: string;
+    //     items: MenuItem[];
+    //   }
+      
+    //   interface Menu {
+    //     firstCourses: MenuSection;
+    //     mainCourses: MenuSection;
+    //     desserts: MenuSection;
+    //   }
+
+    //   let menu: Menu = [{}]
+
+    return (
+        <>
+            <h1>LAST DANCE</h1>
+            <h2>RESTAURANG</h2>
+            <div className="menu">
+                <h3>Menu</h3>
+                <h4>Förrätt</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quasi?</p>
+                <div>199kr</div>
+                <h4>Varmrätt</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quasi?</p>
+                <div>349kr</div>
+                <h4>Efterrätt</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, quasi?</p>
+                <div>149kr</div>
+            </div>
+        </>
+    )
 }
