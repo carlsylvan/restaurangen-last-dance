6598a2cbb307982ac954b258aca5b321e3de7cd5
ehnvik <gustav.ehnvik@gmail.com>
2023-03-15 14:54:19 +0100

    Fixed validation time choosing button bug

--- src/components/SelectBookingTime/SelectBookingTime.tsx
+++ src/components/SelectBookingTime/SelectBookingTime.tsx
@@ -2,41 +2,44 @@ import { useState } from "react";
 import { BookingTimeWrapper } from "../styled/Wrappers";
 
 interface IBookingTime {
-    bookingTime: string,
-    isSelected: boolean,
+  bookingTime: string;
+  isSelected: boolean;
 }
 
 interface ISelectBookingTimeProps {
-    handleBookingTime(bookingTime:string):void,
-    isTableAvailable:boolean,
-
+  handleBookingTime(bookingTime: string): void;
+  isTableAvailable: boolean;
 }
 
-export const SelectBookingTime = (props:ISelectBookingTimeProps) => {
-    const [time, setTime] = useState<IBookingTime[]>([
-        {bookingTime: "17:00", isSelected:false},
-        {bookingTime: "21:00", isSelected:false},
-    ]);
-    const handleClick = (item:IBookingTime) => {
-        props.handleBookingTime(item.bookingTime);
-        let options = [...time];
-        options.map((o)=>o.isSelected=false);
-        options[options.findIndex((o)=>o.bookingTime===item.bookingTime)].isSelected=
-        !options[options.findIndex((o)=>o.bookingTime===item.bookingTime)].isSelected;
+export const SelectBookingTime = (props: ISelectBookingTimeProps) => {
+  const [time, setTime] = useState<IBookingTime[]>([
+    { bookingTime: "17:00", isSelected: true },
+    { bookingTime: "21:00", isSelected: false },
+  ]);
+  const handleClick = (item: IBookingTime) => {
+    props.handleBookingTime(item.bookingTime);
+    let options = [...time];
+    options.map((o) => (o.isSelected = false));
+    options[
+      options.findIndex((o) => o.bookingTime === item.bookingTime)
+    ].isSelected =
+      !options[options.findIndex((o) => o.bookingTime === item.bookingTime)]
+        .isSelected;
     setTime(options);
-    }
-   
-    
-    const html = time.map((item,i)=>{
-        return (
-            <BookingTimeWrapper key={i} onClick={()=>{handleClick(item)}} selected = {item.isSelected} isAvailable = {props.isTableAvailable}>
-                {item.bookingTime}
-            </BookingTimeWrapper>
-        )
-    })
+  };
+
+  const html = time.map((item, i) => {
     return (
-        <>
-            {html}
-        </>
-    )
-}
+      <BookingTimeWrapper
+        key={i}
+        onClick={() => {
+          handleClick(item);
+        }}
+        selected={item.isSelected}
+        isAvailable={props.isTableAvailable}>
+        {item.bookingTime}
+      </BookingTimeWrapper>
+    );
+  });
+  return <>{html}</>;
+};
