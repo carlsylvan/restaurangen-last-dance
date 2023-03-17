16f85da2bc97e1e7a5bd20522fdafd0226f78895
sergejsvoronins <svoronins@gmail.com>
2023-03-17 12:58:30 +0100

    gdpr updated

--- src/components/gdpr/Gdpr.tsx
+++ src/components/gdpr/Gdpr.tsx
@@ -1,4 +1,4 @@
-import { GdprInputWrapper, GdprWrapper } from "../styled/Wrappers"
+import { GdprInputWrapper} from "../styled/Wrappers"
 
 interface IGdprProps {
     changeGdprStatus(): void
@@ -11,7 +11,7 @@ export const Gdpr = (props:IGdprProps) => {
     }
     return (
         <>
-            <p>Vi samlar in ditt namn och din e-postadress för att bekräfta din bordsbokning och skicka dig ett bekräftelsemail. Genom att klicka på “Jag godkänner” samtycker du till vår användning av din personliga information som beskrivs i vår integritetspolicy. Du kan ändra dina preferenser eller dra tillbaka ditt samtycke när som helst.</p>
+            <span>Vi samlar in ditt namn och din e-postadress för att bekräfta din bordsbokning och skicka dig ett bekräftelsemail. Genom att klicka på “Jag godkänner” samtycker du till vår användning av din personliga information som beskrivs i vår integritetspolicy. Du kan ändra dina preferenser eller dra tillbaka ditt samtycke när som helst.</span>
             <GdprInputWrapper >
                 <label htmlFor="gdpr">Jag godkänner</label>
                 <input onChange={handleChange} type="checkbox" id="gdpr"/>
--- src/components/styled/Wrappers.tsx
+++ src/components/styled/Wrappers.tsx
@@ -263,13 +263,39 @@ export const AdminWrapper = styled.div`
 `;
 
 export const GdprWrapper = styled.div`
+  display:${(props:IGdprWrapperProps)=>props.showGdpr ? "flex" : "none"};
+  flex-direction: column;
+  position: absolute;
   box-shadow: 0 0 5px rgba(0,0,0,0.4);
   background-color: white;
-  display:${(props:IGdprWrapperProps)=>props.showGdpr ? "block" : "none"};
-  position: absolute;
   bottom: 0;
   z-index: 6;
-  width: 100%;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 0.6rem;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    width: 100%;
+  }
+  span {
+    width:100%;
+    font-size: 12px;
+    @media screen and (min-width: 768px) {
+    width: 80%;
+    }
+  }
 `;
 export const GdprInputWrapper = styled.div`
+  width:100%;
+  @media screen and (min-width: 768px) {
+    width: 20%;
+    }
+ label {
+  box-shadow: 0 0 5px rgba(0,0,0,0.4);
+  padding: 0.6rem;
+ }
+ input {
+  display: none;
+ }
 `;
